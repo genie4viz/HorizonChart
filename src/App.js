4944712c83c47889c2ca6a6b4b7242c03f00446b
selistym <selistym@protonmail.com>
2019-04-04 03:16:50 -0800

    init#03

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
-import HorizonGraph from './components/HorizonGraph';
+import logo from './logo.svg';
 import './App.css';
-
-const params = [
-  {
-      "Value": "B+",
-      "ValueStyle": {
-          "Margin": 10,
-          "TextSize": 80
-      },
-      "HexStyle": {
-          "Margin": 10,
-          "Radius": 20
-      },
-      "startColor": "red",
-      "endColor": "green",
-      "fillerColor": "#041e44",
-      "endAngle": 120,
-      "startAngle": 0,
-      "arc_w": 17,
-      "arc_h": 10
-  }
-];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HorizonGraph params={params[0]}/>        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
